a492e94553b5a21af4d328376623f9e485bdfd98
Pawan <poojary.pawan16@gmail.com>
2019-12-26 16:00:30 +0530

    modal finished

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,8 +3,8 @@ var btn = document.getElementsByClassName("plan__btn");
 var backdrop = document.getElementsByClassName("backdrop");
 var rem_bdrop = document.getElementsByClassName("modal--no");
 var modal = document.getElementById("modal");
-console.log(rem_bdrop[0]);
-console.log(modal);
+// console.log(rem_bdrop[0]);
+// console.log(modal);
 
 // for (var i=0; i<= 2; i++){
 //    btn.push(list[i]);
@@ -23,12 +23,9 @@ console.log(modal);
     
 // };
 for (var i=0; i< btn.length; i++){
-    console.log(btn[i]);
-
         btn[i].addEventListener("click", function(){
             backdrop[0].style.display = "block";
             modal.style.display = "block";
-            console.log("I tried master");
           });
 
     
@@ -38,5 +35,4 @@ for (var i=0; i< btn.length; i++){
 rem_bdrop[0].addEventListener("click",function(){
     backdrop[0].style.display ="none";
     modal.style.display = "none";
-    console.log("is it working")
 });

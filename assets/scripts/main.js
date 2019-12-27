3dfacdb41959ca48a9a96824762bec43aa1c3497
Pawan <poojary.pawan16@gmail.com>
2019-12-27 13:50:54 +0530

    mobile nav added not responsive yet

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,25 +4,39 @@ var backdrop = document.getElementsByClassName("backdrop");
 var rem_bdrop = document.getElementsByClassName("modal--no");
 var modal = document.getElementById("modal");
 var mobut = document.getElementsByClassName("hamburger");
+var mobnav = document.getElementsByClassName("mobile-nav");
 // console.log(rem_bdrop[0]);
 // console.log(modal);
 
-// for (var i=0; i<= 2; i++){
-//    btn.push(list[i]);
-//    console.log(list[i]);
-// }
+/*for (var i=0; i<= 2; i++){
+   btn.push(list[i]);
+   console.log(list[i]);
+}
 
-// for (var i=0; i< btn.length; i++){
-//     console.log(btn[i]);
-//     (function(index){
-//         btn[index].addEventListener("click", function(){
-//             backdrop[0].style.display = "block";
-//             modal.style.display = "block";
-//             console.log("I tried master");
-//           });
-//     })(i);
+for (var i=0; i< btn.length; i++){
+    console.log(btn[i]);
+    (function(index){
+        btn[index].addEventListener("click", function(){
+            backdrop[0].style.display = "block";
+            modal.style.display = "block";
+            console.log("I tried master");
+          });
+    })(i);
     
-// };
+};*/
+// Hamburger Mobile menu
+
+mobut[0].addEventListener("click",function(){
+    mobnav[0].style.display = "block";
+    backdrop[0].style.display = "block";
+})
+
+backdrop[0].addEventListener("click",function(){
+    this.style.display = "none";
+    mobnav[0].style.display = "none";
+})
+
+// CHOOSE PLAN + BACKDROP
 for (var i=0; i< btn.length; i++){
         btn[i].addEventListener("click", function(){
             backdrop[0].style.display = "block";
@@ -37,3 +51,4 @@ rem_bdrop[0].addEventListener("click",function(){
     backdrop[0].style.display ="none";
     modal.style.display = "none";
 });
+
